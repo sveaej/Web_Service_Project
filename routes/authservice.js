a5bb088c932ced01ec8ee6c09c5b7c70c48606d0
sveaej <147221418+sveaej@users.noreply.github.com>
2025-01-23 12:08:56 -0500

    started on userauth post

--- routes/authservice.js
+++ routes/authservice.js
@@ -1,9 +1,18 @@
 const key = require("../platformkey");
-
+const userOps = require("../operations/userops");
+const jsend = require("../utils/jsend"); //not sure what this is
 //GET
 
 
 //POST
-app.post('/users', [
-    //create user here
-])
+app.post('/api/users', (req, res) => {
+    //authenticate
+    if (req.header.Eleos-Platform-Key != key) {
+        return res.status(401).send(
+            jsend(401, {
+                message: "Platform key is incorrect.",
+            })
+        );
+    }
+    //now do the stuff
+});
