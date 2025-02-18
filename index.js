2ae6be7500c46ea993681f6adf8dce7cb9490fc6
sveaej <147221418+sveaej@users.noreply.github.com>
2025-02-18 11:07:54 -0500

    Each endpoint checks the auth header on each request with middleware

--- index.js
+++ index.js
@@ -1,87 +1,43 @@
 /*
-This file creates a simple local web server.
+This file creates a simple local web server and receives API calls.
 */
 
 const express = require("express")
 const Auth = require("./routes/authservice")
 const Loads = require("./routes/loadservice")
+const key = "testval"
 
 const app = express()
 const port = 3000
 
 app.use(express.urlencoded({extended: true}))
 
+//Middleware to cause each request to authenticate with the token
+app.use(function (req, res, next) {
+    const auth = req.headers['eleos-platform-key']
+    if (!auth) {
+        return res.status(401).json({error: "No authorization header!"})
+    }
+    else if (auth != key) {
+        console.log(auth)
+        console.log(key)
+        return res.status(401).json({error: "Authorization header value is incorrect!"})
+    }
+    next()
+})
+
 app.get('/authenticate/:token', (req, res) => {
-    //res.send("This is the get request for /authenticate!")
     Auth.verifyLogin(req, res)
-    //req.params.token
 })
 
 app.post('/authenticate', (req, res) => {
-    //res.send("This is the post request for /authenticate!");
     Auth.loginUser(req, res)
 })
 
 app.get('/loads', (req, res) => {
-    //stuff goes here
     res.send("This is the get request for /loads!")
 })
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}!`)
 })
-
-/*
-const http = require("http")
-
-//const mongoose = require("mongoose");
-//const express = require("express");
-
-//const User = require("./models/user");
-//const Load = require("./models/load");
-
-//Connect to the local mongoose database
-//mongoose.connect("mongodb://localhost/eleoswsp");
-
-//Create a server
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.write("Server response")
-    res.end();
-})
-
-server.listen((3000), () => {
-    console.log(`Server is running!`);
-})
-*/
-
-/*
-Okay, here are my questions
-
-*/
-
-//This is where I will put the endpoint stuff. I might want to divide my CRUD functions
-//into different files as well 
-
-/*
-run() 
-async function run() {
-    
-    
-    const user = User.create({ 
-        full_name: "Test3", 
-        api_token: "invalidtoken3", 
-        web_token: "idek3", 
-        billing_code: "lol1234_oo3", 
-        billing_description: "This is a billing description for user Test3",
-        billing_type: "per-load",
-        dashboard_code: "DEFAULT_DASHBOARD",
-        username: "T3",
-    })
-    
-    await (await user).save()
-    
-    console.log(user);
-}
-    */
