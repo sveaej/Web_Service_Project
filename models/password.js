33743ef3f432e0f52498c61703be90fea0e20cd8
sveaej <147221418+sveaej@users.noreply.github.com>
2025-02-26 10:22:19 -0500

    Created password model, updated authservice to work with it

---
+++ models/password.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/eleoswsp");
+
+const passwordSchema = new mongoose.Schema({
+    username: { //must be unique - I could add validation for this later if I have time
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+})
+
+module.exports = mongoose.model('Passwords', passwordSchema);
--- routes/authservice.js
+++ routes/authservice.js
@@ -1,6 +1,7 @@
 const key = require("../platformkey");
 const mongoose = require("mongoose");
 const User = require("../models/user");
+const Password = require("../models/password");
 //const util = require("util"); //for debugging
 
 //Connect to the database
@@ -11,13 +12,22 @@ exports.loginUser = async (req, res) => {
     //res.send(`The username is ${req.body.username}`)
     //Create a new token
     var token = "123abc"
-    //Find the user by username
-    const user = await User.findOne({username: req.body.username})
-    //Update the token
-    user.api_token = token;
-    await user.save();
-    //return that user
-    res.json(user.toJSON())
+    //validate the password first
+    console.log(`The password is ${req.body.password}\n`);
+    const login = await Password.findOne({username: req.body.username});
+    if (req.body.password == login.password) {
+        //User is authenticated
+        //Find the user by username
+        const user = await User.findOne({username: req.body.username})
+        //Update the token
+        user.api_token = token;
+        await user.save();
+        //return that user
+        return res.json(user.toJSON());
+    }
+    else {
+        return res.status(401).json({error: "Password is incorrect."});
+    }
 }
 
 //AUTHENTICATE - GET
